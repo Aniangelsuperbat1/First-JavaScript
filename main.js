1d30713b2325e7487c61cb2a112f0ed26f9956c6
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-12-21 20:47:40 -0500

    Added loops text.

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-console.log("hello world");
-let home = "NYC"
-console.log(home)
+// // console.log("hello world");
+// // let home = "NYC"
+// // console.log(home)
+
+// // write your code below!
+// function happyHolidays() {
+//     return ("Happy holidays!")
+//   }
+  
+//   function happyHolidaysTo(name) {
+//     return ("Happy Holidays, ${name}")
+//   }
+  
+//   happyHolidaysTo("Patrick")
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`
+  }
+
+  say("hello", "pat")
